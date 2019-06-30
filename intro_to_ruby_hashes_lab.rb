5b59aeaf568cd305ba87c40435c01af5bb3dad2b
Patrick Harrison <jahman@bellatlantic.net>
2019-06-30 18:20:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,10 @@ def monopoly_with_second_tier
    	monopoly = {
 	        :railroads => {
 	        :pieces =>
+	}       
 	        :rent_in_dollars{},
       :names =>{}, 
+end      
       :shortline =>{}
  } 
 } 
