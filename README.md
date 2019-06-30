6f38aeb5d9c5d6d814ccd4c4f83b155822606815
Patrick Harrison <jahman@bellatlantic.net>
2019-06-30 18:11:30 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ monopoly =  {
       {
         "mortgage_value"=>"$200"
       },
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
         "mortgage_value"=>"$400"
       },
--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,28 +1,64 @@
 def monopoly
-	monopoly = {
-	  :railroads =>{pieces: =>4,rent_in_dollars: => {one_piece_owned: =>25,two_pieced_owned =>50,three_pieces_owned =>100,four_oieces_owned =>200},
-    :names =>{reading_railroad =>{"mortgage_value" =>"$100"},
-    :pennysylvania_railroad =>{"mortgage_valule"=>"$200"},
-    :b_nad_o_railroad =>"mortgage_value" =>"$400"},
-    :short_lines =>{"mortgage_value"=>"$800"}
-    }
-	}
-	  
-	}
-
+  # describe "Building a Nested Monopoly Hash" do
+  # describe "Adding Key/Value Pairs in the Top Level" do
+  #   describe "monopoly" do
+  #     it "adds a key/value pair to the monopoly hash. The key should be :railroads and the value should be an empty hash" do
+  #       expect(monopoly[:railroads]).to eq({})
+  #       expect(monopoly.keys.count).to eq(1)
+  #       expect(monopoly.values).to match_array([{}])
+  #     end
+ 	monopoly = {
+	      :railroads => {} 
+	     
+	 } 
 end
-
+	 
 def monopoly_with_second_tier
+   	monopoly = {
+	      :railroads => {} 
+	     
+	  } 
+end      
+# 	       :pieces
+# 	       :rent_in_dollars 
+# 	       {
+# 	         :one_piece_owned =>25,
+# 	         :two_pieced_owned =>50,
+# 	         :three_pieces_owned =>100,
+# 	         :four_pieces_owned =>200
+# 	         	       }
+#           :names 
+#           {
+#             :reading_railroad =>
+#             {
+#               "mortgage_value" =>"$100"
+               
+#             },
+#     :pennysylvania_railroad =>
+#     {
+#       "mortgage_valule"=>"$200"
+      
+#     },
+#       :b_and_o_railroad =>
+#       {
+#       "mortgage_value" => "$400"
+             
+#         },  
+#       :short_lines =>
+#       {
+#         "mortgage_value"=>"$800"
+         
+#       }
+    
+# 	}
+	  
+# end
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
-end
 
-def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
-end
 
-def monopoly_with_fourth_tier
+
+
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
-end
